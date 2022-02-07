ebadf134081cfebebc88c03d7e8c64d31670b6c6
Mackenzie Bauer <mabauer@ualberta.ca>
2022-02-06 21:17:13 -0700

    Initial javascript click removes all text.

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggle(id) {
+    let button = document.getElementById(id);
+    if (button.style.display == 'block') {
+        button.style.display = 'none';
+    } else {
+        button.style.display = 'block';
+    }
+}
