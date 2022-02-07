c66873502891dca35391c80af41f58948089bced
Mackenzie Bauer <mabauer@ualberta.ca>
2022-02-06 21:50:26 -0700

    Finished HTML file for tracker.

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-function toggle(id) {
-    let button = document.getElementById(id);
-    if (button.style.display == 'block') {
-        button.style.display = 'none';
-    } else {
-        button.style.display = 'block';
-    }
+// getting the names and age from user with the
+// help of prompt
+let name = prompt('What is your name?');
+let age = prompt('What is your age?');
+let entryAge = 18;
+let seniorAge = 60;
+
+// printing the age to the console
+alert(`Your name is ${name} and you are ${age} years old.`);
+
+// using conditional if-else
+if( (age > entryAge) && (age <= seniorAge)){
+// console.log('Welcome to La La Land!');
+swal({
+    title: "Great!",
+    text: "Welcome to La La Land!",
+    icon: "success",
+    });
+}else if(age > seniorAge){
+// console.log('Your ride is free..Have a good Day!');
+    swal({
+    title: "Awesome!",
+    text: "Welcome Sir/Mam to La La Land!",
+    icon: "info",
+    });
+}else{
+//console.log('Sorry.. buddy.')
+swal({
+    title: "Oops!",
+    text: "Sorry Buddy!",
+    icon: "warning",
+    });
 }
